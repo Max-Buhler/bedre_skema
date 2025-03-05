fc2c5fa457ae252506b1780aa0c0ef48d09c0b7c
Muskelmax <maxbuhler50@gmail.com>
2025-03-05 18:43:10 +0100

    Added basic scrapper class, which can scrape the skema-page of lectio

--- scraper.py
+++ scraper.py
@@ -1,2 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+sessionId = os.getenv('SESSION_ID')
+autoLoginKey = os.getenv('AUTO_LOGIN_KEY')
+
+
+class Scraper:
+  def __init__(self, sessionId, autoLoginKey, URL):
+    self.cookies = {
+      "ASP.NET_SessionId": sessionId,
+      "autologinkeyV2": autoLoginKey,
+    }
+    self.URL = URL
+    self.soup = self.getHTML()
+    print(self.soup)
+
+  def getHTML(self):
+    response = requests.get(self.URL, cookies=self.cookies)
+    response.encoding = "utf-8"
+    clean_text = clean_text = response.text.lstrip("\ufeff")
+    soup = BeautifulSoup(clean_text, "html.parser")
+    return soup
+
+
+
+
+
+LectioScraper = Scraper(sessionId, autoLoginKey, "https://www.lectio.dk/lectio/518/SkemaNy.aspx")
+
+
 
 
