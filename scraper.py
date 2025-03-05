d15dca1da531865fd3404c7c014ed94b475fda81
Muskelmax <maxbuhler50@gmail.com>
2025-03-05 19:04:15 +0100

    isolated the module-info from skema-page

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from dotenv import load_dotenv
 import os
+import re
 load_dotenv()
 sessionId = os.getenv('SESSION_ID')
 autoLoginKey = os.getenv('AUTO_LOGIN_KEY')
@@ -15,7 +16,6 @@ class Scraper:
     }
     self.URL = URL
     self.soup = self.getHTML()
-    print(self.soup)
 
   def getHTML(self):
     response = requests.get(self.URL, cookies=self.cookies)
@@ -23,13 +23,20 @@ class Scraper:
     clean_text = clean_text = response.text.lstrip("\ufeff")
     soup = BeautifulSoup(clean_text, "html.parser")
     return soup
+  
+  def getClasses(self):
+    for module in self.soup.findAll("a", attrs={"class":"s2skemabrik"}):
+      moduel_text = module.get("data-tooltip", "")
+      if moduel_text:
+        print(moduel_text)
+
 
 
 
 
 
 LectioScraper = Scraper(sessionId, autoLoginKey, "https://www.lectio.dk/lectio/518/SkemaNy.aspx")
-
+LectioScraper.getClasses()
 
 
 
