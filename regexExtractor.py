475907d4aab67c6e80d8c6e958d26de2797f2fbf
Muskelmax <maxbuhler50@gmail.com>
2025-03-17 16:19:19 +0100

    tilføjede kommentarer og fiksede nogle små fejl

--- regexExtractor.py
+++ regexExtractor.py
@@ -2,24 +2,35 @@ import re
 
 class RegexExtractor:
   def __init__(self):
-    self.dateRegex = r"\d{1,2}/\d{1,2}-2025"
-    self.timeRegex = r"\d{1,2}/\d{1,2}-2025 (\d{1,2}:\d{2}) til (\d{1,2}:\d{2})"
-    self.teacherRegex = r"Lærer: ([A-Z][a-z]+(?:\s[A-Z][a-z]+)*) \(([a-z]{1,4})\)"
-    self.roomRegex = r"Lokale: ([A-Z][0-9]{3}s)"
-    self.teamRegex = r"Hold: ([A-Z][0-9][a-z] [A-Za-z]{2,3})"
+    #De forskællige regexes defineres
+    self.__dateRegex = r"\d{1,2}/\d{1,2}-2025"
+    self.__timeRegex = r"\d{1,2}/\d{1,2}-2025 (\d{1,2}:\d{2}) til (\d{1,2}:\d{2})"
+    self.__teacherRegex = r"Lærer: ([A-Z][a-z]+(?:\s[A-Z][a-z]+)*) \(([a-z]{1,4})\)"
+    self.__roomRegex = r"Lokale: ([A-Z][0-9]{3}s)"
+    self.__teamRegex = r"Hold: ([A-Z][0-9][a-z] [A-Za-z]{2,3})"
+    self.__cancelledRegex = r"Aflyst!" 
+    self.__noteRegex = r"Note:(.*)"
+    self.__homeworkRegex = r"Lektier:(.*?)(Øvrigt indhold|Note|$)"
+    self.__otherRegex = r"Øvrigt indhold:(.*?)(Lektier|Note|$)"
 
+  #de specifikke informationer bliver fundet i html-stringen via regexene og __extractGroup metoden, hvorefter de bliver returneret som en dictionary
   def getData(self, indhold):
       result = {
-        "date": self._extract_group(self.dateRegex, indhold, 0),
-        "timeFrom": self._extract_group(self.timeRegex, indhold, 1),
-        "timeTo": self._extract_group(self.timeRegex, indhold, 2),
-        "teacher": self._extract_group(self.teacherRegex, indhold, 1),
-        "room": self._extract_group(self.roomRegex, indhold, 0),
-        "team": self._extract_group(self.teamRegex, indhold, 0),
+        "date": self.__extractGroup(self.__dateRegex, indhold, 0),
+        "timeFrom": self.__extractGroup(self.__timeRegex, indhold, 1),
+        "timeTo": self.__extractGroup(self.__timeRegex, indhold, 2),
+        "teacher": self.__extractGroup(self.__teacherRegex, indhold, 1),
+        "room": self.__extractGroup(self.__roomRegex, indhold, 1),
+        "team": self.__extractGroup(self.__teamRegex, indhold, 1),
+        "cancelled": True if re.search(self.__cancelledRegex, indhold) else False,
+        "note": self.__extractGroup(self.__noteRegex, indhold, 1),
+        "homework": self.__extractGroup(self.__homeworkRegex, indhold, 1),
+        "other": self.__extractGroup(self.__otherRegex, indhold, 1)
       }
       return result
 
-  def _extract_group(self, regex, text, group_num):
+  #En metode, som tester om regexen har fundet et match og returner værdien, hvis den er fundet (den extraherer altså regexens output)
+  def __extractGroup(self, regex, text, group_num):
       """Helper function to safely extract a regex match group."""
-      match = re.search(regex, text)
+      match = re.search(regex, text, re.DOTALL)
       return match.group(group_num) if match else None
--- scraper.py
+++ scraper.py
@@ -3,24 +3,26 @@ import requests
 
 class Scraper:
   def __init__(self, cookies):
-    self.cookies = cookies
+    self.__cookies = cookies
 
-
+  #lectio's html-kode findes og defineres til __soup
   def getHTML(self, URL, week, year):
+    #via betingelser findes der ud af om år og uge er defineret
     if(week is None):
-      response = requests.get(URL, cookies=self.cookies)
+      response = requests.get(URL, cookies=self.__cookies)
     else:
       if(year is None):
-        response = requests.get(URL + "?week=" + week + "2025", cookies=self.cookies)
+        response = requests.get(URL + "?week=" + week + "2025", cookies=self.__cookies)
       else:
-        response = requests.get(URL + "?week=" + week + year, cookies=self.cookies)
-    response.encoding = "utf-8"
+        response = requests.get(URL + "?week=" + week + "2025", cookies=self.__cookies)
+    #html-koden laves om til en string
     clean_text = response.text.encode("utf-8", "ignore").decode("utf-8")
-    self.soup = BeautifulSoup(clean_text, "html.parser")
+    self.__soup = BeautifulSoup(clean_text, "html.parser")
 
   def getModules(self):
     modules = []
-    for module in self.soup.findAll("a", attrs={"class":"s2skemabrik", "class": "s2bgbox"}):
+    #html-modul-elementerne bliver udvalgt via deres class-attribut
+    for module in self.__soup.findAll("a", attrs={"class":"s2skemabrik", "class": "s2bgbox"}):
       moduel_text = module.get("data-tooltip", "")
       if moduel_text:
         modules.append(moduel_text)
